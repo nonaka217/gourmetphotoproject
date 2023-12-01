930dcd630097038b08fb707ea70025228ecd3ec0
nina <ktq2390316@stu.o-hara.ac.jp>
2023-12-01 12:15:17 +0900

    Commit

--- gourmetphoto/urls.py
+++ gourmetphoto/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('post/', views.CreatePhotoView.as_view(), name='post'),
     path('post_done', views.PostSuccessView.as_view(), name='post_done'),
     path('gourmetphoto/<int:Category>', views.CategoryView.as_view(), name = 'gourmetphotos_cat'),
-    path('user-list/<int:user>', views.UserView.as_view(), name = 'user_list'),
+    # path('user-list/<int:user>', views.UserView.as_view(), name = 'user_list'),
+    path('photo-detail/<int:pk>', views.DetailView.as_view(), name = 'photo_detail'),
+    path('mypage/', views.MypageView.as_view(), name = 'mypage'),
+    path('gourmetphoto/<int:pk>/delete/', views.PhotoDeleteView.as_view(), name = 'photo_delete'),
 ]
--- gourmetphoto/views.py
+++ gourmetphoto/views.py
@@ -6,6 +6,8 @@ from .forms import GourmetphotoPostForm
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from .models import GourmetphotoPost
+from django.views.generic import DetailView
+from django.views.generic import DeleteView
 
 class IndexView(ListView):
     template_name = 'gourmetphoto/index.html'
@@ -36,11 +38,31 @@ class CategoryView(ListView):
         Categories = GourmetphotoPost.objects.filter(Category=Category_id).order_by('-posted_at')
         return Categories
     
-class UserView(ListView):
-    template_name = 'gourmetphoto/index.html'
-    paginate_by = 9
+# class UserView(ListView):
+    # template_name = 'gourmetphoto/index.html'
+    # paginate_by = 9
 
+    # def get_queryset(self):
+        # user_id = self.kwargs['user']
+        # user_list = GourmetphotoPost.objects.filter(user=user_id).order_by('-posted_at')
+        # return user_list
+    
+class DetailView(DetailView):
+    template_name = 'gourmetphoto/detail.html'
+    model = GourmetphotoPost
+
+class MypageView(ListView):
+    template_name = 'gourmetphoto/mypage.html'
+    paginate_by = 9
+    
     def get_queryset(self):
-        user_id = self.kwargs['user']
-        user_list = GourmetphotoPost.objects.filter(user=user_id).order_by('-posted_at')
-        return user_list
+        queryset =GourmetphotoPost.objects.filter(user=self.request.user).order_by('-posted_at')
+        return queryset
+    
+class PhotoDeleteView(DeleteView):
+    model = GourmetphotoPost
+    template_name = 'gourmetphoto/photo_delete.html'
+    success_url = reverse_lazy('gourmetphoto:mypage')
+    
+    def delete(self, request, *args, **kwargs):
+        return super().delete(request, *args, **kwargs)
